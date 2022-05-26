d44f1d1eaba9208863c72dfe220a4efab1ca1cdb
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-26 22:25:21 +0400

    add sass, react-router, font-family

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Header from '../components/Header'
+
+const Home = () => {
+	return (
+		<section className='home'>
+			<Header />
+		</section>
+	)
+}
+
+export default Home
